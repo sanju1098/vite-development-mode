09ef264a369c893c24727dd81fc6a8821aea06a4
sanju1098 <kumar.sanjay101998@gmail.com>
2024-04-12 23:04:28 +0530

    resolved lint errors

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,7 @@
 import { defineConfig, UserConfig } from "vite";
 
-// https://vitejs.dev/config/
-// Define the main Vite config
-export default ({ mode }: { mode: string }): UserConfig => {
-	return defineConfig({
-		...require(`./vite/vite.config.${mode}.ts`).default,
-	});
+// Define the main Vite config - "https://vitejs.dev/config/"
+export default async ({ mode }: { mode: string }): Promise<UserConfig> => {
+	const configModule = await import(`./vite/vite.config.${mode}.ts`);
+	return defineConfig(configModule.default);
 };
